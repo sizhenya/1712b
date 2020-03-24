7125a207d31452d149ac7af559fd5b346d7f6ac9
whd <sizhenya@qq.com>
2020-03-24 20:50:53 +0800

    第一次提交

---
+++ src/main/java/day_01/Hello.java
@@ -0,0 +1,11 @@
+package day_01;
+/*
+*
+*@author 司振亚
+*@date 2020年3月20日---下午1:50:46
+*
+*/
+public class Hello {
+	
+}
+
---
+++ src/test/java/com/bw/TestHello.java
@@ -0,0 +1,11 @@
+package com.bw;
+/*
+*
+*@author 司振亚
+*@date 2020年3月20日---下午1:51:16
+*
+*/
+public class TestHello {
+
+}
+
